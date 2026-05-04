5db81d0aecc13949e05c39905162aee2cd2de466
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-05-04 14:56:21 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

--- patchBaileys.js
+++ patchBaileys.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
     try {
         execSync(
-            'npm install @whiskeysockets/baileys@npm:toxic-baileys@latest --no-audit --no-fund --prefer-online --legacy-peer-deps',
+            'npm install @whiskeysockets/baileys@npm:toxic-baileys@1.0.6 --no-audit --no-fund --prefer-online --legacy-peer-deps',
             { cwd: __dirname, stdio: 'inherit' }
         );
     } catch (e) {}
