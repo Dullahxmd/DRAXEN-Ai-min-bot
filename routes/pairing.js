583be89e6e93cfb9614a77898ddf07bd9bda3a20
Dullahxmd <ahmedisultanii3@gmail.com>
2026-05-04 08:32:53 +0300

    Update pairing.js

--- routes/pairing.js
+++ routes/pairing.js
@@ -2,13 +2,13 @@
 
   const express = require('express');
 
-  module.exports = function createPairingRouter(ToxicPair) {
+  module.exports = function createPairingRouter(DraxenPair) {
       const router = express.Router();
 
       router.get('/', async function (req, res) {
           const number = req.query.number;
           if (!number) return res.status(400).json({ error: 'Number required.' });
-          ToxicPair(number, res).catch(function () {
+          DraxenPair(number, res).catch(function () {
               if (!res.headersSent) res.status(500).json({ error: 'Internal error' });
           });
       });
